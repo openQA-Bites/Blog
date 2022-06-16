32491c10a6e9d170dec15da6eed3199165a444fd
phoenix <felix.niederwanger@suse.com>
2022-06-16 15:42:55 +0200

    Update create_post.sh script
    Update the create_post script to work with better formatted program
    arguments.

--- create_post.sh
+++ create_post.sh
@@ -5,30 +5,63 @@
 
 # Directory for input
 POSTS="content/posts"
-TEMPLATE="$POSTS/_template.md"
+TEMPLATE=".template.md"
+FOLDER=0
+TITLE=""
 
-if [[ $# -gt 0 ]]; then
-	TITLE="$@"
-else
+function usage() {
+	echo "$0 [-f] [-t title]"
+	echo "   -f             Create post in new folder"
+	echo "   -t title       Define title"
+}
+
+# Parse arguments
+while getopts "hft:l:" opt; do
+	case $opt in
+		h)
+			usage
+			exit 0
+			;;
+		f)
+			FOLDER=1
+			;;
+		t)
+			TITLE="$OPTARG"
+			;;
+		:)
+			echo "Option -$opt requires an argument" >&2
+			exit 1
+			;;
+	esac	
+done
+
+if [[ $TITLE == "" ]]; then
 	printf "New post title: "
 	read TITLE
 fi
-
 if [[ $TITLE == "" ]]; then
-	echo "No title given"
+	echo "error: no title" >&2
 	exit 1
 fi
 
-set -e
-#set -x
-
 DATE=`date --iso`
 # 2021-00-00T00:00:00+01:00
-TIMESTAMP="`date +%Y-%m-%d`T`date +%H:%M:%S`+01:00"
-URL="/`date +%Y`/`date +%m`/`date +%d`/$TITLE/"
+#TIMESTAMP="`date +%Y-%m-%d`T`date +%H:%M:%S`+02:00"
+TIMESTAMP=`date +%Y-%m-%dT%H:%M:%S%:z`
+DATESTAMP=`date +%Y-%m-%d`
+URL="/`date +%Y`/`date +%m`/`date +%d`/${TITLE// /-}/"
+POST="$DATESTAMP-${TITLE// /_}"
 
-FILENAME="`date +%Y-%m-%d`.md"
-FILE="$POSTS/$FILENAME"
+
+if [[ $FOLDER == 1 ]]; then
+	POSTDIR="${POSTS}/${POST}"
+	FILE="$POSTDIR/index.md"
+	echo "$POSTDIR"
+	mkdir -p "$POSTDIR"
+else
+	POSTDIR="${POSTS}"
+	FILE="$POSTDIR/$DATESTAMP-${TITLE// /_}.md"
+fi
 
 if [[ -s "$FILE" ]]; then
 	echo "File $FILE exists already"
